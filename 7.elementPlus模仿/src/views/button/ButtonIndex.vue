8a048dd7ccb5adaab2ed5ab5568ca5ac666232cb
付祖敏 <1326580471@qq.com>
2022-06-12 22:48:05 +0800

    基础样式

--- 7.elementPlus模仿/src/views/button/ButtonIndex.vue
+++ 7.elementPlus模仿/src/views/button/ButtonIndex.vue
@@ -5,3 +5,19 @@ console.log(1);
 <template>
   <button>button</button>
 </template>
+
+<style>
+button {
+  line-height: 1;
+  height: 32px;
+  white-space: nowrap;
+  cursor: pointer;
+  text-align: center;
+  box-sizing: border-box;
+  transition: 0.1s;
+  padding: 8px 15px;
+  background-color: #fff;
+  border-radius: 4px;
+  border: 1px solid #d9d9d9;
+}
+</style>
