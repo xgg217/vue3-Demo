b91f2f1a7a3dc264986899fef9421abe4a81583d
付祖敏 <1326580471@qq.com>
2022-06-12 23:05:31 +0800

    引入elementPlus组件库

--- 7.elementPlus模仿/src/main.ts
+++ 7.elementPlus模仿/src/main.ts
@@ -4,8 +4,12 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router/router";
 
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
 const app = createApp(App);
 
+app.use(ElementPlus);
 app.use(createPinia());
 app.use(router);
 
