300c6431e48ef288e2cc19e59ffe2ef2821786ad
付祖敏 <1326580471@qq.com>
2022-06-13 23:11:28 +0800

    优化代码

--- 7.elementPlus模仿/src/views/button/ButtonIndex.vue
+++ 7.elementPlus模仿/src/views/button/ButtonIndex.vue
@@ -4,30 +4,10 @@ import ButtonCmp from "./Cmp/ButtonCmp.vue";
 
 <template>
   <!-- 自定义 -->
-  <ButtonCmp>12</ButtonCmp>
+  <ButtonCmp type="info">12</ButtonCmp>
 
   <!-- 组件 -->
   <el-button>Default</el-button>
 </template>
 
-<style>
-button {
-  line-height: 1;
-  height: 32px;
-  white-space: nowrap;
-  cursor: pointer;
-  text-align: center;
-  box-sizing: border-box;
-  transition: 0.1s;
-  padding: 8px 15px;
-  background-color: #fff;
-  border-radius: 4px;
-  border: 1px solid #d9d9d9;
-  color: rgb(96, 98, 102);
-  font-size: 14px;
-}
-button span {
-  display: inline-flex;
-  align-items: center;
-}
-</style>
+<style></style>
