80c5f4040a723c8c692b2ed6c31fbd4aab310a9c
付祖敏 <1326580471@qq.com>
2022-06-21 23:15:51 +0800

    学习

--- 4.vue3再次学习/src/views/HomesViews.vue
+++ 4.vue3再次学习/src/views/HomesViews.vue
@@ -1,34 +1,16 @@
 <template>
   <div>homes页面</div>
-  <p>{{ val }}</p>
+  <!-- <p>{{ val }}</p> -->
   <!-- <button @click="handleAdd"> 点击</button>
   <button @click="handlerShow">显示/隐藏</button> -->
+  <p>{{ text }}</p>
+  <input v-model="text">
 </template>
 
 <script setup lang="ts">
+import { useDebouncedRef } from './useDebouncedRef';
 
-import { ref, toRef, reactive } from 'vue';
-const val  = ref(1);
-// const isBool = ref(false);
-const info = reactive({
-  name: '小刚刚',
-  age: 12,
-});
-
-const ageRef = toRef(info, 'age');
-
-// 更改该 ref 会更新源属性
-ageRef.value = 13;
-console.log(info.age); // 13
-// 一同更新了info的age属性
-console.log(info.age); // 13
-
-// 更改源属性也会更新该 ref
-info.age = 18;
-console.log(ageRef.value); // 18
-
-
-
+const text = useDebouncedRef('xgg', 300);
 
 
 
