43d5afb4115f999e042d8eccf6613dfe3daa3fb0
付祖敏 <1326580471@qq.com>
2022-06-23 19:02:17 +0800

    测试

--- 4.vue3再次学习/src/App.vue
+++ 4.vue3再次学习/src/App.vue
@@ -7,11 +7,6 @@
 //   title: string
 //   year?: number
 // }
-
-// const book = reactive<Book>({ title: 'Vue 3 指引' });
-
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 </script>
 
 
