e670fcdb1a7e2d421b6064e6d9cf5f6398f364bb
付祖敏 <1326580471@qq.com>
2022-06-23 18:56:54 +0800

    格式化

--- 4.vue3再次学习/vite.config.ts
+++ 4.vue3再次学习/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,4 +20,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
