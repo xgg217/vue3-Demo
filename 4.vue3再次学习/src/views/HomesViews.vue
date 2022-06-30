431a1f2c28939d8fff6dd214e6e8044bf6a51d0d
付祖敏 <1326580471@qq.com>
2022-06-30 19:11:29 +0800

    测试

--- 4.vue3再次学习/src/views/HomesViews.vue
+++ 4.vue3再次学习/src/views/HomesViews.vue
@@ -25,22 +25,22 @@ setTimeout(() => {
 }, 2000);
 
 (function() {
-   const data = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body><GetWebUrl xmlns="http://tempuri.org/"><JsonData>{  "UserId" : 10194,  "Version" : "1.0.1",  "Language" : 3,  "MasterSecret" : "CTGi*7d54Fs*ssxH8*Rtg3dtrEa2K8d===Yhdt6",  "UserToken" : "7D30994BE0A3BC82757C0E1A671740BA3635C0C9A016F288B02CA7E8D24026BD58B7B46903A803E9E6725F6C31BD6DEF8A0F7E17663788990C08673AB3FB2CC2A0310C5B5C10E3F0",  "ClientType" : 1,  "UrlType" : 7,  "AppMerchantId" : "49E47C84BCD177E3",  "AppEstateId" : "CF2A3FC76F4D80E6",  "DeviceInfo" : "iPad|15.3.1|1.0.0"}</JsonData></GetWebUrl></soap:Body></soap:Envelope>';
+  const data = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body><GetWebUrl xmlns="http://tempuri.org/"><JsonData>{  "UserId" : 10194,  "Version" : "1.0.1",  "Language" : 3,  "MasterSecret" : "CTGi*7d54Fs*ssxH8*Rtg3dtrEa2K8d===Yhdt6",  "UserToken" : "7D30994BE0A3BC82757C0E1A671740BA3635C0C9A016F288B02CA7E8D24026BD58B7B46903A803E9E6725F6C31BD6DEF8A0F7E17663788990C08673AB3FB2CC2A0310C5B5C10E3F0",  "ClientType" : 1,  "UrlType" : 7,  "AppMerchantId" : "49E47C84BCD177E3",  "AppEstateId" : "CF2A3FC76F4D80E6",  "DeviceInfo" : "iPad|15.3.1|1.0.0"}</JsonData></GetWebUrl></soap:Body></soap:Envelope>';
   axios.post('/api/MerchantService.svc', data, {
 
-        headers: {
-          'Content-Type': 'text/xml',
-          'SOAPAction': 'http://tempuri.org/IMerchantService/GetWebUrl',
-          'Accept': 'application/json;text/javascript;*/'
-        }
-      })
-      .then(function (response) {
-        alert(response.status);
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    headers: {
+      'Content-Type': 'text/xml',
+      'SOAPAction': 'http://tempuri.org/IMerchantService/GetWebUrl',
+      'Accept': 'application/json;text/javascript;*/',
+    },
+  })
+    .then(function (response) {
+      alert(response.status);
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 })();
 
 
