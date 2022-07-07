d2fb17c60f0f8f83daee85b108ee59de472f3493
付祖敏 <1326580471@qq.com>
2022-07-07 23:42:33 +0800

    测试

--- 4.vue3再次学习/src/views/User/MyUser.vue
+++ 4.vue3再次学习/src/views/User/MyUser.vue
@@ -1,14 +1,21 @@
 <template>
   <div>User1</div>
   <div>{{ obj.age }}</div>
+  <p ref="root"></p>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 
 const obj = reactive({
   name: 'xgg',
   age: 12,
+});
+
+const root = ref(null);
+
+onMounted(() => {
+  console.log(root.value);
 });
 
 
