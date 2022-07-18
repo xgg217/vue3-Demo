9df0cf966b514c0a19bf15daea5ffc44d9153558
付祖敏 <1326580471@qq.com>
2022-07-18 23:13:50 +0800

    初始化与代理设置

---
+++ 万年历/vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://v.juhe.cn/",
+        changeOrigin: true, // 开发模式，默认的origin是真实的 origin:localhost:3000 代理服务会把origin修改为目标地址
+        ws: true, // 是否代理websockets
+        secure: false, // 是否https接口
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
