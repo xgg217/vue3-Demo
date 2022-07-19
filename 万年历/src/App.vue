9d65b58717a726587bee3ae9f28f94168b05e967
付祖敏 <1326580471@qq.com>
2022-07-19 23:44:05 +0800

    路由初始化

--- 万年历/src/App.vue
+++ 万年历/src/App.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import { Tabbar, TabbarItem } from "vant";
-import { ref } from "vue";
-
-const active = ref(0);
 </script>
 
 <template>
   <RouterView />
 
-  <van-tabbar v-model="active">
-    <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-    <van-tabbar-item icon="search">标签</van-tabbar-item>
-    <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-    <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+  <van-tabbar route>
+    <van-tabbar-item replace to="/" icon="home-o">当天</van-tabbar-item>
+    <van-tabbar-item replace to="/month" icon="search">近期</van-tabbar-item>
+    <van-tabbar-item replace to="/year" icon="friends-o">当年</van-tabbar-item>
   </van-tabbar>
 </template>
 
