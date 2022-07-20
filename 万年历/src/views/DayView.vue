3e791e6ed432d4394678783dafee302a4e3a333f
付祖敏 <1326580471@qq.com>
2022-07-21 00:07:18 +0800

    页面开发

--- 万年历/src/views/DayView.vue
+++ 万年历/src/views/DayView.vue
@@ -1,7 +1,30 @@
-<!-- <script setup lang="ts">
+<script setup lang="ts">
+import { NavBar, ConfigProvider } from "vant";
+// import { apiPostDay } from "@/api/request";
 
-</script> -->
+// (() => {
+//   apiPostDay("2015-1-1")
+//     .then((res) => {
+//       console.log(res);
+//       // res.data.
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// })();
+</script>
 
 <template>
-  <div>day</div>
+  <van-nav-bar title="标题" class="day-nav" />
 </template>
+
+<style scoped>
+.day-nav {
+  background-color: red;
+}
+
+.day-nav /deep/ .van-nav-bar__title {
+  color: #fff;
+  font-size: 20px;
+}
+</style>
