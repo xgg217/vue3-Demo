c186244aae02164f87392347eb136515dac9ba30
付祖敏 <1326580471@qq.com>
2022-07-23 17:06:39 +0800

    样式开发

--- 万年历/src/views/DayView/IndexView.vue
+++ 万年历/src/views/DayView/IndexView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Cell, CellGroup } from "vant";
 import HeaderNav from "@/components/HeaderNav.vue";
 import DateSearch from "./cmp/DateSearch.vue";
 
@@ -10,11 +11,15 @@ const handleGetDate = (dataStr: string) => {
 <template>
   <HeaderNav title="当天信息" />
   <DateSearch @change="handleGetDate" />
+  <van-cell-group>
+    <van-cell title="单元格" value="内容" />
+    <van-cell title="单元格" value="内容" />
+  </van-cell-group>
 </template>
 
 <style scoped>
 .content {
   height: 20vh;
-  border: 1px solid red;
+  /* border: 1px solid red; */
 }
 </style>
