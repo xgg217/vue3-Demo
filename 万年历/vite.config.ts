81b459d53ee2ab287c1d3226f2aec00483077065
付祖敏 <1326580471@qq.com>
2022-07-27 15:11:42 +0800

    增加解构props功能

--- 万年历/vite.config.ts
+++ 万年历/vite.config.ts
@@ -9,7 +9,9 @@ import { VantResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true,
+    }),
     Components({
       resolvers: [VantResolver()],
     }),
