0ad774308e96e363e00b6cc641a35b9f6a111482
付祖敏 <1326580471@qq.com>
2022-11-06 17:26:39 +0800

    调整

--- 组合api学习/src/views/HomeView.vue
+++ 组合api学习/src/views/HomeView.vue
@@ -6,16 +6,7 @@ import { RouterLink } from "vue-router";
   <main>
     <ul>
       <li>
-        <RouterLink to="/OpJsq">计数器传统方式</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/CmJsq">计数器组合API</RouterLink>
-      </li>
-      <li>
         <RouterLink to="/setTitle">修改网页标题</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/ScrollPosition">监听页面滚动位置</RouterLink>
       </li>
     </ul>
   </main>
--- 组合api学习/src/views/useTitle/index.vue
+++ 组合api学习/src/views/useTitle/index.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+// import useTitle from './useTitle';
+const myTitle = ref('')
+const title = ref('')
 </script>
 
 <template>
-  <div class="aaa c">useTitle</div>
+  <div class="aaa c">
+    <input type="text" v-model="myTitle">
+    <div>----------VueUse库------</div>
+    <input type="text" v-model="title">
+  </div>
 </template>
 
 <style scoped>
