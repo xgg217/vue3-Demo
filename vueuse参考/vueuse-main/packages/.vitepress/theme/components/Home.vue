b6e4840d1c67a847fb1beb3fa2ac0ddbe3d475dd
付祖敏 <1326580471@qq.com>
2022-11-09 09:44:44 +0800

    参考

---
+++ vueuse参考/vueuse-main/packages/.vitepress/theme/components/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HomeHero from './HomeHero.vue'
+import HomeFeatures from './HomeFeatures.vue'
+import HomeFooter from './HomeFooter.vue'
+</script>
+
+<template>
+  <div class="home" mt-4 flex flex-col items-center>
+    <HomeTeam />
+
+    <HomeSponsors />
+
+    <HomeContributors />
+  </div>
+</template>
