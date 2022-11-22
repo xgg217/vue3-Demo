2533e3ab80560cbbcfd816dd6cb9a15465700b97
付祖敏 <1326580471@qq.com>
2022-11-22 16:46:11 +0800

    初始化

---
+++ 1.hook学习/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <ul>
+    <li>
+      <RouterLink to="/table">自定义指令-表格的封装</RouterLink>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ 1.hook学习/src/views/table/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>表格操作</div>
+</template>
+
+<style lang="scss" scoped>
+</style>
