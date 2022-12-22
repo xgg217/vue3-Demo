3bc267d871f3136aa225a9aaf4dbd5eff7506686
付祖敏 <1326580471@qq.com>
2022-12-22 14:19:44 +0800

    初始化

---
+++ 2.穿梭框/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TransferCmp from './components/TransferCmp/index.vue'
+</script>
+
+<template>
+
+
+  <main>
+    <TransferCmp />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ 2.穿梭框/src/components/TransferCmp/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const leftList = ref([]) // 左侧列表所有数据
+const leftSelectedList = ref([]) // 左侧列表选中数据
+const rightList = ref([]) // 右侧列表所有数据
+const rightSelectedList = ref([]) // 右侧列表选中数据
+</script>
+
+<template>
+  <div>111</div>
+</template>
+
+<style lang="scss" scoped>
+</style>
