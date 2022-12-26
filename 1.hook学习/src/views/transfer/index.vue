b9cb84faec8ce5f9cd92ce078360f4f8a2d6f923
付祖敏 <1326580471@qq.com>
2022-12-26 14:51:52 +0800

    格式调整

--- 1.hook学习/src/views/transfer/index.vue
+++ 1.hook学习/src/views/transfer/index.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { IItem } from './types'
+
+const generateData = () => {
+  const data: Option[] = []
+  for (let i = 1; i <= 15; i++) {
+    data.push({
+      key: i,
+      label: `Option ${i}`,
+      disabled: i % 4 === 0,
+    })
+  }
+  return data
+}
+
+const data = ref<IItem[]>(generateData())
+const value = ref([])
+
+
+
 import TransferCmp from './components/TransferCmp.vue'
 </script>
 
 <template>
-  <TransferCmp />
+  <TransferCmp :data="data" />
 </template>
 
 <style lang="scss" scoped>
--- 1.hook学习/src/views/transfer/types.ts
+++ 1.hook学习/src/views/transfer/types.ts
@@ -1,6 +1,6 @@
-export type itemType = {
+export interface IItem {
   key: number; // 唯一标识
   label: string; // 显示的文本
   disabled: boolean; // 是否禁用
-  checked: boolean; // 是否选中
+  checked?: boolean; // 是否选中
 }
