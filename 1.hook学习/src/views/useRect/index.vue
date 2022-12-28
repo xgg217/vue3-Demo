caff84fe137d934eda540a66559b334382520e27
付祖敏 <1326580471@qq.com>
2022-12-28 17:39:28 +0800

    获取元素位置

--- 1.hook学习/src/views/useRect/index.vue
+++ 1.hook学习/src/views/useRect/index.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import useRect1 from './useRect';
+import { useRect } from '@vant/use';
+
+const root = ref();
+
+onMounted(() => {
+  const rect = useRect(root.value);
+  const rect1 = useRect1(root.value);
+  console.log(rect);
+  console.log(rect1);
+});
+
 </script>
 
 <template>
-  <div>useRect</div>
+  <div ref="root">useRect</div>
 </template>
 
 <style lang="scss" scoped>
