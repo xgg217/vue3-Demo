60ece3667e960c8996ffb9230759707d5c67b8e4
付祖敏 <fuzumin@foxmail.com>
2023-01-04 23:29:24 +0800

    开发

--- 1.hook学习/src/views/useCountDown/index.vue
+++ 1.hook学习/src/views/useCountDown/index.vue
@@ -1,19 +1,38 @@
 <script setup lang="ts">
 import { useCountDown } from '@vant/use';
+import useCountDowns from './useCountDown';
 
+const TIME = 23 *60 * 60 * 1000
+
+// vant 提供的倒计时
 const { current } = (() => {
    const countDown = useCountDown({
       // 倒计时 24 小时
-      time: 60 * 60 * 1000,
+      time: TIME,
     });
 
     // 开始倒计时
-    countDown.start();
+    // countDown.start();
 
   return {
     current: countDown.current,
   }
-})()
+})();
+
+//  自定义倒计时
+const {
+  newCurrent
+} = (() => {
+  const countDown = useCountDowns({
+    // 倒计时 24 小时
+    time: TIME,
+  });
+
+  return {
+    newCurrent: countDown.current
+  }
+
+})();
 </script>
 
 <template>
@@ -30,12 +49,12 @@ const { current } = (() => {
 
   <div class="box">
     <p>自定义 提供</p>
-    <span>总时间：{{ current.total }}</span>
-    <span>剩余天数：{{ current.days }}</span>
-    <span>剩余小时：{{ current.hours }}</span>
-    <span>剩余分钟：{{ current.minutes }}</span>
-    <span>剩余秒数：{{ current.seconds }}</span>
-    <span>剩余毫秒：{{ current.milliseconds }}</span>
+    <span>总时间：{{ newCurrent.total }}</span>
+    <span>剩余天数：{{ newCurrent.days }}</span>
+    <span>剩余小时：{{ newCurrent.hours }}</span>
+    <span>剩余分钟：{{ newCurrent.minutes }}</span>
+    <span>剩余秒数：{{ newCurrent.seconds }}</span>
+    <span>剩余毫秒：{{ newCurrent.milliseconds }}</span>
   </div>
 </template>
 
--- 1.hook学习/src/views/useCountDown/useCountDown.ts
+++ 1.hook学习/src/views/useCountDown/useCountDown.ts
@@ -1,1 +1,79 @@
-export default function useCountDown() {}
+import type { ComputedRef } from "vue";
+
+type UseCountDownOptions = {
+  time: number; // 总时间 单位毫秒
+  // millisecond?: boolean;
+  // onChange?: (current: CurrentTime) => void;
+  // onFinish?: () => void;
+};
+
+type CurrentTime = {
+  total: number; // 总时间
+  days: number; // 天
+  hours: number; // 小时
+  minutes: number; // 分钟
+  seconds: number; // 秒
+  milliseconds: number; // 毫秒
+};
+
+type CountDown = {
+  start: () => void; // 开始计时
+  pause: () => void; // 暂停计时
+  reset: (totalTime: number) => void; // 重置计时
+  // current: ComputedRef<CurrentTime>;
+  current: CurrentTime; // 当前时间
+};
+
+// 时间处理
+const formatTime = (time: number) => {
+  const days = Math.floor(time / 1000 / 60 / 60 / 24);
+  const hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  const minutes = Math.floor((time / 1000 / 60) % 60);
+  const seconds = Math.floor((time / 1000) % 60);
+  const milliseconds = Math.floor((time % 1000) / 10);
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+    total: time,
+  };
+}
+
+export default function useCountDown(options: UseCountDownOptions):CountDown {
+
+  const current = {
+    days: 0,
+    hours: 0,
+    total: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+  }
+
+  console.log(formatTime(options.time));
+  const { days, hours, minutes, seconds, milliseconds, total } = formatTime(options.time);
+  current.days = days;
+  current.hours = hours;
+  current.minutes = minutes;
+  current.seconds = seconds;
+  current.milliseconds = milliseconds;
+  current.total = total;
+
+  // 开始计时
+  const start = () => {}
+
+  // 暂停计时
+  const pause = () => {}
+
+  // 重置计时
+  const reset = (totalTime: number) => {}
+
+  return {
+    start,
+    pause,
+    reset,
+    current
+  }
+}
