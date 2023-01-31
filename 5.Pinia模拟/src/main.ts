d894d93f29c25f55cc90582668aae384d65ee004
付祖敏 <fuzumin@foxmail.com>
2023-01-31 22:49:43 +0800

    开发

--- 5.Pinia模拟/src/main.ts
+++ 5.Pinia模拟/src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import router from './router/index';
 import './assets/reset.css';
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+
+
+createApp(App).use(router).use(pinia).mount('#app')
--- 5.Pinia模拟/src/views/HomesViews.vue
+++ 5.Pinia模拟/src/views/HomesViews.vue
@@ -3,6 +3,10 @@
 </template>
 
 <script setup lang="ts">
+import { useTodoListStore } from '@/store/todoList'
+
+const todoListStore = useTodoListStore()
+console.log(todoListStore);
 
 </script>
 
