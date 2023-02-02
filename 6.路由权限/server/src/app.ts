5bb8bc585db29f8e1f0677b643eea4ec1511a13b
付祖敏 <fuzumin@foxmail.com>
2023-02-02 18:33:40 +0800

    调整

--- 6.路由权限/server/src/app.ts
+++ 6.路由权限/server/src/app.ts
@@ -6,8 +6,16 @@ const PORT = 8081;
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.post('/postTest', (req, res)=>{
+interface IBody {
+  uid: number
+}
+
+// 获取所有路由
+app.post('/user_router_list', (req, res)=>{
   console.log('post请求体传参',req.body);
+
+  const { uid } = req.body as IBody;
+
   res.send('post请求测试');
 });
 
