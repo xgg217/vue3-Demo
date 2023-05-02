f9604a744187904b4ceac63179730ae163370396
付祖敏 <fuzumin@foxmail.com>
2023-05-03 00:23:04 +0800

    调整

--- three学习/demo/20230427/model.js
+++ three学习/demo/20230427/model.js
@@ -7,8 +7,16 @@ const material = new THREE.MeshPhongMaterial( { color: 0x00ff00, shininess: 100 
 
 //创建一个空的几何体对象
 const mesh = new THREE.Mesh(geometry, material);
-const mesh2 = new THREE.Mesh(geometry, material);
+// const mesh2 = new THREE.Mesh(geometry, material);
+const mesh2 = mesh.clone();
+
+mesh2.geometry = mesh.geometry.clone();
+mesh2.material = mesh.material.clone();
+
+// mesh.material.color.set(0xffff00);
 
 mesh2.position.x = 100;
+
+mesh2.material.color.set(0xffff00);
 
 export{ mesh, mesh2 };
