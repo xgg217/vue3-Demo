97065691711459351ac18b0fed03fdc0e0033128
付祖敏 <fuzumin@foxmail.com>
2023-06-01 23:15:35 +0800

    调整

--- three学习/demo/20230520/model.js
+++ three学习/demo/20230520/model.js
@@ -1,13 +1,15 @@
-import { Mesh,MeshBasicMaterial, TextureLoader, PlaneGeometry } from "three";
+import { Mesh,MeshBasicMaterial,DoubleSide, TextureLoader, PlaneGeometry, BoxGeometry, CircleGeometry } from "three";
 
 // 创建一个几何体
-const geometry = new PlaneGeometry(100, 100);
+const geometry = new CircleGeometry(100);
+// const geometry = new BoxGeometry(100, 100, 100);
 
 const textLoader = new TextureLoader();
 
 const texture = textLoader.load('./路飞.jpg');
 const material = new MeshBasicMaterial({
-  map: texture
+  map: texture,
+  side: DoubleSide
 });
 
 const mesh = new Mesh(geometry, material)
