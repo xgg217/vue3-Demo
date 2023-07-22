47862bd022ea50c8715bb9e397bbc79d7b870bc6
付祖敏 <fuzumin@foxmail.com>
2023-07-22 23:17:53 +0800

    调整

--- three学习/demo/20230718/model.js
+++ three学习/demo/20230718/model.js
@@ -1,25 +1,36 @@
 import * as THREE from "three";
-
-const geometry = new THREE.BoxGeometry(50, 50, 50);
-const material = new THREE.MeshLambertMaterial({
-  color: 0x00ffff,
-});
-const mesh = new THREE.Mesh(geometry, material);
+import dataArr from "./data.js"
 // 将几何体添加到场景中
 
+const geometruy = (() => {
+  const pointsArr = [];
+  dataArr.forEach(e => {
+    const v2 = new THREE.Vector2(e[0],e[1])
+    pointsArr.push(v2);
+  })
+  const shape = new THREE.Shape(pointsArr);
+  return new THREE.ShapeGeometry(shape);
+  // return geometruy
+})();
 
+const material =  new THREE.MeshLambertMaterial({
+  color: 0x00ffff,
+  side: THREE.DoubleSide
+})
+
+const mesh = new THREE.Mesh(geometruy, material)
 
 // 包围盒
-(() => {
-  const scale = new THREE.Vector3()
-  const box3 = new THREE.Box3()
-  console.log('box3',box3);
-  
-  box3.expandByObject(mesh); // 计算模型包围盒
-  
-  box3.getSize(scale)
-  
-  console.log('模型包围盒尺寸', scale);
-})();
+// (() => {
+//   const scale = new THREE.Vector3()
+//   const box3 = new THREE.Box3()
+//   console.log('box3',box3);
+//
+//   box3.expandByObject(mesh); // 计算模型包围盒
+//
+//   box3.getSize(scale)
+//
+//   console.log('模型包围盒尺寸', scale);
+// })();
 
 export default mesh;
