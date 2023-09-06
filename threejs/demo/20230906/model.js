5cfded8ceb64e37807860040f0ab71fb8283c65d
付祖敏 <fuzumin@foxmail.com>
2023-09-06 22:36:46 +0800

    学习

--- threejs/demo/20230906/model.js
+++ threejs/demo/20230906/model.js
@@ -1,31 +1,37 @@
 import * as THREE from 'three';
-// 引入CSS3模型对象CSS3DObject
-import {
-    CSS3DObject
-} from 'three/addons/renderers/CSS3DRenderer.js';
+// 引入gltf模型加载库GLTFLoader.js
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+// 引入CSS3模型或精灵对象
+import {CSS3DObject,CSS3DSprite} from 'three/addons/renderers/CSS3DRenderer.js';
 
-const geometry = new THREE.ConeGeometry(25, 80);
-geometry.translate(0, 40, 0); //y轴正方向，平移高度一半
-const material = new THREE.MeshLambertMaterial({
-    color: 0x00ffff,
+const loader = new GLTFLoader(); //创建一个GLTF加载器
+const model = new THREE.Group();
+
+//需要批量标注的标签数据arr
+const arr = ['设备A','设备B','停车场'];
+
+loader.load("./工厂.glb", function (gltf) {
+    model.add(gltf.scene);
+    // const tag = document.getElementById('tag');
+    // console.log(JSON.parse(JSON.stringify(tag)))
+    arr.forEach(item => {
+        
+        // 注意是多个标签，需要克隆复制一份
+        const div = document.getElementById('tag').cloneNode();
+        div.innerHTML = item;//标签数据填写
+        
+        // HTML元素转化为threejs的CSS3对象
+        // const tag = new CSS3DObject(div);
+        const tag = new CSS3DSprite(div);
+        div.style.pointerEvents = 'none'; //避免标签遮挡canvas鼠标事件
+        // obj是建模软件中创建的一个空对象
+        const obj = gltf.scene.getObjectByName(item+'标注');
+        console.log(JSON.parse(JSON.stringify(obj)))
+        //tag会标注在空对象obj对应的位置
+        obj.add(tag);
+        tag.scale.set(0.1,0.1,1);//适当缩放模型标签
+        tag.position.y = 40/2*0.1;//标签底部箭头和空对象标注点重合：偏移高度像素值一半*缩放比例
+    })
 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(50, 0, 50);
 
-// 可视化模型的局部坐标系
-const axesHelper = new THREE.AxesHelper(100);
-mesh.add(axesHelper);
-
-const div = document.getElementById('tag');
-// HTML元素转化为threejs的CSS3模型对象
-const tag = new CSS3DObject(div);
-//标签tag作为mesh子对象，默认受到父对象位置影响
-mesh.add(tag);
-tag.position.y += 80; //圆锥mesh局部坐标系原点在自己底部时候，标签需要向上偏移圆锥自身高度
-const group = new THREE.Group();
-group.add(mesh);
-
-tag.scale.set(0.5,0.5,1);//缩放标签尺寸
-tag.position.y += 10;//累加标签高度一半，标签底部和圆锥顶部标注位置重合
-
-export default group;
+export default model;
