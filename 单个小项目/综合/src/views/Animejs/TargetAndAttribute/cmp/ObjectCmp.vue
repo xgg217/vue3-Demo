ff0d7335e286bf9d9847b46e47dfc6237916135a
付祖敏 <1326580471@qq.com>
2025-01-23 18:34:36 +0800

    调整

--- 单个小项目/综合/src/views/Animejs/TargetAndAttribute/cmp/ObjectCmp.vue
+++ 单个小项目/综合/src/views/Animejs/TargetAndAttribute/cmp/ObjectCmp.vue
@@ -9,13 +9,7 @@
       Infected Files: <span class="infected-count">0</span>
     </h2>
 
-    <div class="but">
-      <!-- <el-button type="primary" @click="onBlue">蓝色下落</el-button>
-      <el-button type="primary" @click="onBlueRed">蓝色红色下落</el-button>
-      <el-button type="primary" @click="onEven">偶数行下落</el-button>
-      <el-button type="primary" @click="onNotRed">非红下落</el-button>
-      <el-button type="primary" @click="onReset">重置</el-button> -->
-    </div>
+    <div class="but"></div>
   </div>
 </template>
 
@@ -28,32 +22,25 @@ const obj = {
   infectedFiles: 8,
 };
 
+const row = reactive({
+  filesScanned: 0,
+  infectedFiles: 0,
+});
+
 const animeRow: TAnimeInstance = {
   filesScanned: null,
   infectedFiles: null,
 };
 
 const init = () => {
-  animeRow.blue = anime({
-    targets: ".box1 li:nth-child(3n + 1)",
+  animeRow.filesScanned = anime({
+    targets: ".box3 .scan-count",
     translateY: 400,
     autoplay: false,
   });
 
-  animeRow.blueRed = anime({
-    targets: [".box1 li:nth-child(3n + 1)", ".box1 li:nth-child(3n + 2)"],
-    translateY: 400,
-    autoplay: false,
-  });
-
-  animeRow.even = anime({
-    targets: ".box1 li:nth-child(even)",
-    translateY: 400,
-    autoplay: false,
-  });
-
-  animeRow.notRed = anime({
-    targets: ".box1 li:not(.box1 li:nth-child(3n + 2))",
+  animeRow.infectedFiles = anime({
+    targets: ".box3 .infected-count",
     translateY: 400,
     autoplay: false,
   });
