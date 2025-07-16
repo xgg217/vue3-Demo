4a57bcc8da5e60273c870ca046ecfe9f17b342ad
付祖敏 <1326580471@qq.com>
2025-07-16 18:24:54 +0800

    调整

--- 单个小项目/综合/src/views/ECharts/SetOption/index.vue
+++ 单个小项目/综合/src/views/ECharts/SetOption/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <TitleCmp />
+    <LegendCmp />
+    <!-- <TitleCmp /> -->
 
     <!-- 柱状图 -->
     <!-- <BarCmp /> -->
@@ -13,7 +14,8 @@
 <script setup lang="ts">
 // import BarCmp from "./components/BarCmp.vue";
 // import PieCmp from "./components/PieCmp.vue";
-import TitleCmp from "./components/TitleCmp.vue";
+// import TitleCmp from "./components/TitleCmp.vue";
+import LegendCmp from "./components/LegendCmp.vue";
 </script>
 
 <style lang="scss" scoped></style>
