d55a2db7049ee8ff979a1873b379271d0fcf2154
xgg217 <38310416+xgg217@users.noreply.github.com>
2025-07-16 23:50:13 +0800

    调整

--- 单个小项目/综合/src/views/ECharts/SetOption/index.vue
+++ 单个小项目/综合/src/views/ECharts/SetOption/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <LegendCmp />
+    <GridCmp />
+    <!-- <LegendCmp /> -->
     <!-- <TitleCmp /> -->
 
     <!-- 柱状图 -->
@@ -15,7 +16,8 @@
 // import BarCmp from "./components/BarCmp.vue";
 // import PieCmp from "./components/PieCmp.vue";
 // import TitleCmp from "./components/TitleCmp.vue";
-import LegendCmp from "./components/LegendCmp.vue";
+// import LegendCmp from "./components/LegendCmp.vue";
+import GridCmp from "./components/GridCmp.vue";
 </script>
 
 <style lang="scss" scoped></style>
