28487c6fa9a0e731b1d415e64a6aae26d6dd44c3
xgg217 <38310416+xgg217@users.noreply.github.com>
2025-08-05 23:39:50 +0800

    调整

--- 单个小项目/综合/src/views/ECharts/Dataset/index.vue
+++ 单个小项目/综合/src/views/ECharts/Dataset/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <TestCmp />
-    <Test2Cmp />
+    <!-- <TestCmp />
+    <Test2Cmp /> -->
+    <Test3Cmp />
   </div>
 </template>
 
 <script setup lang="ts">
 import TestCmp from "./components/TestCmp.vue";
 import Test2Cmp from "./components/Test2Cmp.vue";
+import Test3Cmp from "./components/Test3Cmp.vue";
 </script>
 
 <style lang="scss" scoped></style>
